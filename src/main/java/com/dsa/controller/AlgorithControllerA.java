fa73ae609d33d77116ed65ce901772c9d16a5c67
avinashbhilare-bharatnxt <avinash.bhilare@bharatnxt.in>
2025-11-13 22:46:16 +0530

    bubble and selection sort

--- src/main/java/com/dsa/controller/AlgorithControllerA.java
+++ src/main/java/com/dsa/controller/AlgorithControllerA.java
@@ -1,10 +1,104 @@
 package com.dsa.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dsa.constant.DsaAlgoConstants;
+import com.dsa.dto.CommonResponse;
+import com.dsa.service.AlgorithmServiceA;
+import com.dsa.service.DataStructureServiceA;
 
 @RestController
 @RequestMapping("/al-v")
 public class AlgorithControllerA {
 
+	private static Logger logger = LoggerFactory.getLogger(DataStructureControllerA.class);
+	
+	@Autowired
+	AlgorithmServiceA algorithmServiceA;
+	
+	@GetMapping("/v1/ping")
+	public CommonResponse ping() throws Exception {
+		logger.info("Entry in ping");
+		
+		CommonResponse commonResponse = new CommonResponse(HttpStatus.OK.value(), DsaAlgoConstants.SUCCESS, algorithmServiceA.ping());
+		
+		logger.info("Entry in ping");
+		return commonResponse;
+	}
+	
+	/**************************************************************************************************
+	 * ARRAY GETTING AND SETTING
+	 **************************************************************************************************/
+	
+	@PostMapping("/setArray")
+	public CommonResponse setArray(@RequestParam(value = "array") Integer[] array) {
+		logger.info("entry from setArray");
+		
+		CommonResponse commonResponse = null;
+		try {
+			commonResponse = new CommonResponse(HttpStatus.OK.value(), DsaAlgoConstants.SUCCESS, algorithmServiceA.setArray(array));
+		} catch (Exception e) {
+			e.printStackTrace();
+			commonResponse = new CommonResponse(HttpStatus.OK.value(), DsaAlgoConstants.SUCCESS, e.getMessage());
+		}
+		logger.info("exit from setArray");
+		return commonResponse;
+	}
+
+	@GetMapping("/getArray")
+	public CommonResponse getArray() {
+		logger.info("entry in getArray");
+
+		CommonResponse commonResponse = null;
+		try {
+			commonResponse = new CommonResponse(HttpStatus.OK.value(), DsaAlgoConstants.SUCCESS, algorithmServiceA.getArray());
+		} catch (Exception e) {
+			e.printStackTrace();
+			commonResponse = new CommonResponse(HttpStatus.OK.value(), DsaAlgoConstants.SUCCESS, e.getMessage());
+		}
+		logger.info("exit from getArray");
+		return commonResponse;
+	}	
+	
+	/**************************************************************************************************
+	 * SORTING DATA STRUCTURE 
+	 **************************************************************************************************/
+	
+	@GetMapping("/bubble-sort")
+	public CommonResponse bubbleSort() {
+		logger.info("entry in bubbleSort");
+
+		CommonResponse commonResponse = null;
+		try {
+			commonResponse = new CommonResponse(HttpStatus.OK.value(), DsaAlgoConstants.SUCCESS, algorithmServiceA.bubbleSort());
+		} catch (Exception e) {
+			e.printStackTrace();
+			commonResponse = new CommonResponse(HttpStatus.OK.value(), DsaAlgoConstants.SUCCESS, e.getMessage());
+		}
+		logger.info("exit from bubbleSort");
+		return commonResponse;
+	}
+	
+	@GetMapping("/selection-sort")
+	public CommonResponse selectionSort() {
+		logger.info("entry in selectionSort");
+
+		CommonResponse commonResponse = null;
+		try {
+			commonResponse = new CommonResponse(HttpStatus.OK.value(), DsaAlgoConstants.SUCCESS, algorithmServiceA.selectionSort());
+		} catch (Exception e) {
+			e.printStackTrace();
+			commonResponse = new CommonResponse(HttpStatus.OK.value(), DsaAlgoConstants.SUCCESS, e.getMessage());
+		}
+		logger.info("exit from selectionSort");
+		return commonResponse;
+	}
 }
--- src/main/java/com/dsa/service/impl/AlgorithServiceImplA.java
+++ src/main/java/com/dsa/service/impl/AlgorithServiceImplA.java
@@ -1,5 +1,79 @@
 package com.dsa.service.impl;
 
-public class AlgorithServiceImplA {
+import org.springframework.stereotype.Service;
 
+import com.dsa.service.AlgorithmServiceA;
+
+@Service
+public class AlgorithServiceImplA implements AlgorithmServiceA {
+
+	private Integer[] array = {1, 3 ,5, 7, 9, 2, 8, 4, 6};
+	
+	@Override
+	public String ping() throws Exception {
+		// TODO Auto-generated method stub
+		return "success";
+	}
+
+	@Override
+	public String setArray(Integer[] array) throws Exception {
+		this.array = array;
+		return getArray();
+	}
+
+	@Override
+	public String getArray() throws Exception {
+		String response = "";
+		if (array == null) {
+			return "Opps, array is empty..!!!";
+		}
+		for (int i = 0; i < array.length - 1; i++) {
+			response += array[i] + "-->";
+		}
+		response += array[array.length - 1];
+		return response;
+	}
+
+	@Override
+	public String bubbleSort() throws Exception {
+		if (array == null) {
+			return "Opps, array is empty..!!!";
+		}
+		for (int i = 0; i < array.length; i++) {
+			boolean isArraySorted = true;
+			for (int j = 0; j < array.length - i - 1; j++) {
+				if (array[j] > array[j+1]) {
+					int temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+					isArraySorted = false;
+				}
+			}
+			if (isArraySorted) {
+				return "array sorted..!!!\n" + getArray();
+			}
+		}
+		return "array sorted..!!!\n" + getArray();
+	}
+
+	@Override
+	public String selectionSort() throws Exception {
+		if (array == null) {
+			return "Opps, array is empty..!!!";
+		}
+		for (int i = 0; i < array.length - 1; i++) {
+			int minElementIndex = i;
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[minElementIndex] > array[j]) {
+					minElementIndex = j;
+				}
+			}
+			if (minElementIndex != i) {
+				int temp = array[minElementIndex];
+				array[minElementIndex] = array[i];
+				array[i] = temp;
+			}
+		}
+		return "array sorted..!!\n" + getArray();
+	}
 }
